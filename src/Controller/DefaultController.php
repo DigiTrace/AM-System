71c75bd90ad913fa9c831053ed8cf1e8cf528014
Ben Brooksnieder <bbrooksnieder@gmail.com>
2024-11-08 11:27:26 +0100

    Added php cs fixer dependency

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,54 +1,52 @@
 <?php
-   // AM-System
-   // Copyright (C) 2019 Robert Krasowski
-   // This program was created during an internship at DigiTrace GmbH
-   // Read LIZENZ.txt for full notice
-
-   // This program is free software: you can redistribute it and/or modify
-   // it under the terms of the GNU General Public License as published by
-   // the Free Software Foundation, either version 3 of the License, or
-   // (at your option) any later version.
-
-   // This program is distributed in the hope that it will be useful,
-   // but WITHOUT ANY WARRANTY; without even the implied warranty of
-   // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   // GNU General Public License for more details.
-
-   // You should have received a copy of the GNU General Public License
-   // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-   
+/**
+ * AM-System
+ * Copyright (C) 2019 Robert Krasowski
+ * This program was created during an internship at DigiTrace GmbH
+ * Read LIZENZ.txt for full notice
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author Robert Kraswoski
+ */
 
 namespace App\Controller;
 
-//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\Routing\Annotation\Route;
-#use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-
-
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
-use Symfony\Component\HttpFoundation\Request;
-
-
-#neu
-use Symfony\Component\HttpFoundation\RequestStack;
 use App\Entity\Nutzer;
 
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * 
+ */
 class DefaultController extends AbstractController
 {
     
     
     /**
-     * @Route("/", name="homepage")
+     * Show dashboard.
      */
+    #[Route('/', name: 'homepage')]
     public function indexAction(Request $request)
     {
         /*
-         * Startseite des Managementsystems:
+         * Startseite des Managementsystems:io
          */
         return $this->render('default/index.html.twig');
     }
@@ -59,6 +57,7 @@ class DefaultController extends AbstractController
     /**
      * @Route("/changelog", name="changelog")
      */
+    #[Route('/changelog', name: 'changelog')]
     public function showChangelogAction(Request $request)
     {
         
