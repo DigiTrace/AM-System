4be45ab59454a9fd68be466fef9669f9ab959089
Ben Brooksnieder <bbrooksnieder@gmail.com>
2026-04-09 17:11:00 +0200

    Recipe Update: phpunit/phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,25 +11,3 @@ if (method_exists(Dotenv::class, 'bootEnv')) {
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
-
-// $env = 'test';
-// passthru(
-//     sprintf(
-//         'php bin/console doctrine:database:drop --if-exists --force --env=%s',
-//         $env
-//     )
-// );
-
-// passthru(
-//     sprintf(
-//         'php bin/console doctrine:database:create --if-not-exists --env=%s',
-//         $env
-//     )
-// );
-
-// passthru(
-//     sprintf(
-//         'php bin/console doctrine:schema:create --env=%s',
-//         $env
-//     )
-// );
